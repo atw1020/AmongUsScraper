a9e703431da6254e78703429a6b53ff6a7c9cccd
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-08 08:14:29 -0800

    Added Constants file and a graph that shows the size of images of various dimensions

---
+++ src/constants.py
@@ -0,0 +1,51 @@
+"""
+
+Author: Arthur wesley, Gregory Ghiroli
+
+"""
+
+'''
+
+common video resolutions
+
+'''
+res_1080p = (1920, 1080)
+res_720p = (1280, 720)
+res_480p = (848, 480)
+res_360p = (640, 360)
+res_160p = (272, 160)
+
+# resolution of our images
+resolution = res_480p
+
+
+def size(res):
+    """
+
+    calculates the size in kilobytes of a color image of the given dimensions
+
+    :param res: resolution to calculate
+    :return: size of that image
+    """
+
+    return res[0] * res[1] * 3 / 1024
+
+
+def main():
+    """
+
+    main method
+
+    :return:
+    """
+
+    print("resolution, size (kb)")
+    print("1080p", size(res_1080p), sep=", ")
+    print("720p", size(res_720p), sep=", ")
+    print("480p", size(res_480p), sep=", ")
+    print("360p", size(res_360p), sep=", ")
+    print("160p", size(res_160p), sep=", ")
+
+
+if __name__ == "__main__":
+    main()
