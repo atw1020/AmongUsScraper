3dad5891c9dd7bb361c639e51ca029a49b1310d0
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-07 19:10:47 -0800

    Added init_nn function to Game Classifier.py

--- src/Models/Game Classifier.py
+++ src/Models/Game Classifier.py
@@ -5,4 +5,36 @@ Author: Arthur wesley, Gregory Ghiroli
 """
 
 from tensorflow import keras
-from selenium import webdriver
+from tensorflow.keras import layers
+
+
+def init_nn():
+    """
+
+    initializes the neural network
+
+    :return: game classifier neural network
+    """
+
+    input_layer = layers.Input()
+    convolution = layers.Conv3D()(input_layer)
+    dense = layers.Dense()(convolution)
+    output = layers.Dense()(dense)
+
+    return keras.Model(input_layer=input_layer, outputs=output, name="Game Classifier")
+
+
+def main():
+    """
+
+    test method
+
+    :return:
+    """
+
+    model = init_nn()
+    model.summary()
+
+
+if __name__ == "__main__":
+    main()
