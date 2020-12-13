f3facc913794c02ffff59e12ae19808c352c15dc
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-13 08:41:36 -0800

    Started implementing selenium scrapping interface

--- src/python/Data-Collection/Web Scrapper.py
+++ src/python/Data-Collection/Web Scrapper.py
@@ -6,6 +6,47 @@ https://medium.com/swlh/scraping-live-stream-video-with-python-b7154b1fedde
 
 """
 
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from src.python import constants
 
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def init_webdriver():
+    """
+
+    initializes the selenium webdriver
+
+    :return:
+    """
+
+    dimensions = str(constants.dimensions[0]) + ", " + str(constants.dimensions[1])
+
+    caps = DesiredCapabilities.CHROME
+    caps["goog:loggingPrefs"] = {"performance": "ALL"}
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=" + dimensions)
+    options.add_argument("headless")
+
+    driver = webdriver.Chrome(desired_capabilities=caps, options=options)
+    driver.get("twitch.tv")
+
+    return driver
+
+
+def main():
+    """
+
+    main method
+
+    :return: None
+    """
+
+    driver = init_webdriver()
+
+
+if __name__ == "__main__":
+    main()
