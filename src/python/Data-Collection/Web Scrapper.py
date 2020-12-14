df5d830001342dad6f1d6f4719b2232c37376b53
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-13 21:18:52 -0800

    made code capable of downloading .ts files

--- src/python/Data-Collection/Web Scrapper.py
+++ src/python/Data-Collection/Web Scrapper.py
@@ -13,22 +13,24 @@ import re
 from src.python import constants
 
 from twitchdl import twitch
+from twitchdl import download
+
 from twitchdl.commands import _parse_playlists, _get_playlist_by_name, _get_vod_paths
 
 
-def get_base_url(video_id):
+def get_base_url(video_id, access_token=None):
     """
 
     generates the URL of a video with a given ID
 
+    :param access_token: access token for the video
     :param video_id: ID of the video
     :return: generated URL
     """
 
-    # print_out("<dim>Fetching access token...</dim>")
-    access_token = twitch.get_access_token(video_id)
+    if access_token is None:
+        access_token = twitch.get_access_token(video_id)
 
-    # print_out("<dim>Fetching playlists...</dim>")
     playlists_m3u8 = twitch.get_playlists(video_id, access_token)
     playlists = list(_parse_playlists(playlists_m3u8))
     playlist_uri = _get_playlist_by_name(playlists, constants.quality)
@@ -36,19 +38,21 @@ def get_base_url(video_id):
     return re.sub("/[^/]+$", "/", playlist_uri)
 
 
-def get_vods(video_id):
+def get_vods(video_id, access_token=None):
     """
 
     generates a list of all the vods for the video
 
-    :param video_id:
-    :return:
+    :param access_token: access token for the video
+    :param video_id: ID of the video
+    :return: list of vods the video has
     """
 
     start = None
     end = None
 
-    access_token = twitch.get_access_token(video_id)
+    if access_token is None:
+        access_token = twitch.get_access_token(video_id)
 
     playlists_m3u8 = twitch.get_playlists(video_id, access_token)
     playlists = list(_parse_playlists(playlists_m3u8))
@@ -61,6 +65,28 @@ def get_vods(video_id):
     return _get_vod_paths(playlist, start, end)
 
 
+def get_video(url):
+    """
+
+    gets the video from a url
+
+    :param url:
+    :return: size of the video in bytes
+    """
+
+    response = requests.get(url, stream=True, timeout=download.CONNECT_TIMEOUT)
+
+    size = 0
+
+    with open("test.ts", "wb+") as target:
+
+        for chunk in response.iter_content(chunk_size=download.CHUNK_SIZE):
+            target.write(chunk)
+            size += len(chunk)
+
+    return size
+
+
 def main():
     """
 
@@ -70,9 +96,14 @@ def main():
     """
 
     video_id = "829611887"
+    access_token = twitch.get_access_token(video_id)
 
-    print("url of video", video_id, get_base_url(video_id))
-    print("vods", get_vods(video_id))
+    base_url = get_base_url(video_id, access_token)
+
+    print("url of video", video_id, get_base_url(video_id, access_token))
+    print("vods", get_vods(video_id, access_token))
+
+    get_video(base_url + "0.ts")
 
 
 if __name__ == "__main__":
