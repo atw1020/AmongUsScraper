8a7fc41b67e7c65fd72755a1dedf301494c94ae0
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-16 06:56:01 -0800

    created post_game_classifier_handler.py

---
+++ src/web/game_classifier/post_game_classifier_handler.py
@@ -0,0 +1,13 @@
+"""
+
+Author: Arthur wesley
+
+"""
+
+from http.server import BaseHTTPRequestHandler
+
+
+class PostGameClassifierHandler(BaseHTTPRequestHandler):
+
+    def handle(self):
+        pass
