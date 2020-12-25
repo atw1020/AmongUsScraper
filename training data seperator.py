458d72006a951715a45e4280528c5fc6fda6c361
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-24 21:04:10 -0800

    added training data sorter for winner crude data

--- training data seperator.py
+++ training data seperator.py
@@ -38,7 +38,7 @@ def string_hash(st):
     return int(m.hexdigest(), 16)
 
 
-def sort_crude_data():
+def sort_classifier_crude_data():
     """
 
     sorts through "crude" data and puts it into Test data
@@ -80,6 +80,47 @@ def sort_crude_data():
         os.rename(start_path + file, output)
 
 
+def sort_winner_crude_data():
+    """
+
+    sorts through the data for the winner identifier and sorts it into training and test
+    data
+
+    :return: None (renames files)
+    """
+
+    path = os.path.join("Data", "Winner Identifier")
+
+    files = os.listdir(os.path.join(path, "Crude Data"))
+
+    player_names = dict()
+
+    for file in files:
+
+        items = file.split("-")
+
+        video_id = items[1]
+
+        if video_id in player_names:
+            # if we already have the player name cached, use it
+            player_name = player_names[video_id]
+        else:
+            # otherwise, get the player name from the video ID
+            video = twitch.get_video(video_id)
+            player_name = video['channel']['display_name']
+
+            # cache the player name
+            player_names[video_id] = player_name
+
+        if is_name_test(player_name):
+            data_set = "Test Data"
+        else:
+            data_set = "Training Data"
+
+        os.rename(os.path.join(path, "Crude Data", file),
+                  os.path.join(path, data_set, file))
+
+
 def main():
     """
 
@@ -88,13 +129,16 @@ def main():
     :return:
     """
 
-    sort_crude_data()
+    sort_classifier_crude_data()
+    sort_winner_crude_data()
 
+    """
     streamers = ["Blunder", "shofu", "Kara", "Pokimane", "5up", "captainsparklez",
                  "showthyme", "sykkuno", "GUMMI", "hafu"]
 
     for streamer in streamers:
         print(streamer, "is", "in" if is_name_test(streamer) else "not in", "the test set")
+    """
 
 
 if __name__ == "__main__":
