9d6c3a7bac554d047c7b67e789869ce22488795d
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2020-12-28 14:14:12 -0800

    implemented batch based data collector to decrease memory footprint

--- src/Data_Collection/data_collector.py
+++ src/Data_Collection/data_collector.py
@@ -23,18 +23,21 @@ temp_images = os.path.join("Data", "Temp Images")
 
 class DataCollector:
 
-    def __init__(self, video_id, step=2, verbose=True):
+    def __init__(self, video_id, step=2, verbose=True, batch_size=32):
         """
 
         initializes a DataCollector from a specified video ID
 
         :param video_id: ID of the video to seek through
         :param step: step between images
+        :param batch_size: size of the batches that the video is processed in
         """
 
         # copy the parameters into the object
         self.video_id = video_id
         self.step = step
+        self.verbose = verbose
+        self.batch_size = batch_size
 
         # load NNs
         self.classifier = models.load_model(constants.game_classifier)
@@ -51,16 +54,11 @@ class DataCollector:
 
         self.vods = [(vod, 0) for vod in self.vods]
 
-        # initialize the input tensor
-        self.vods_tensor = None
+        # batches object (initially None)
+        self.batches = None
 
-        # initialize the predictions
+        # predictions object
         self.predictions = None
-
-        # initialize the indices of games
-        self.transitions = None
-
-        self.verbose = verbose
 
     def get_image(self, index):
         """
@@ -72,87 +70,73 @@ class DataCollector:
         """
 
         # get the image and assign it
-        image = web_scrapper.get_still_frame(self.url + self.vods[index][0],
+        return web_scrapper.get_still_frame(self.url + self.vods[index][0],
                                              self.vods[index][1])
-        self.vods_tensor[index] = image
 
-    def get_images(self):
+    def get_images_batch(self):
         """
 
-        saves all of the images in the temporary directory
+        puts all of the images into the vods tensor
 
         :return:
         """
 
-        self.vods_tensor = np.empty((len(self.vods),) + constants.dimensions + (3,))
+        self.batches = [self.vods[i:i + self.batch_size]
+                   for i in range(0, len(self.vods), self.batch_size)]
+
+        self.predictions = np.empty((len(self.vods)))
+
+        for index, batch in enumerate(self.batches):
+
+            start_index = self.batch_size * index
+            end_index = start_index + len(batch)
+
+            # get the tensor
+            vods_tensor = self.get_batch(batch, self.step * index)
+
+            # update the predictions
+            self.predictions[start_index:end_index] = np.argmax(self.classifier.predict(vods_tensor), axis=1)
+
+    def get_batch(self, batch, start_index):
+        """
+
+        put the specified batch into the vods_tensor
+
+        :param batch: batch to put into the vods tensor
+        :param start_index: index of the start of the batch
+        :return:
+        """
+
+        vods_tensor = np.empty((len(batch),) + constants.dimensions + (3,))
 
         # todo: implement with a thread pool
 
-        for i in range(len(self.vods)):
+        for i in range(len(batch)):
+            vods_tensor[i] = self.get_image(start_index + i)
 
-            self.get_image(i)
-
-            if i % int(float(len(self.vods)) / 20) == 0 and self.verbose:
-                print(int(float(i) / len(self.vods) * 100), "% complete", sep="")
-
-        print(sys.getsizeof(self.vods_tensor))
-
-    def classify_images(self):
-        """
-
-        classify the images in the vods tensor
-
-        :return: None (updates state
-        """
-
-        if self.vods_tensor is None:
-            self.get_images()
-
-        # use the classifier to predict
-        self.predictions = np.argmax(self.classifier.predict(self.vods_tensor), axis=1)
-
-        return self.predictions
-
-    def save_predictions(self):
-        """
-
-        save the predictions
-
-        :return:
-        """
-
-        if self.predictions is None:
-            self.classify_images()
-
-        for i in range(len(self.vods)):
-
-            output_path = constants.label_ids[self.predictions[i]] + "-" \
-                          + self.video_id + "-" \
-                          + str(i * self.step) + ".jpg"
-
-            save_img(os.path.join(temp_images, output_path), self.vods_tensor[i])
+        return vods_tensor
 
     def get_game_transitions(self):
         """
 
-        gets the indices of the transitions
+        generates a list of every transition in the game
 
-        :return:
+        :return: list of each transition in the game
         """
 
+        # get the predictions if we don't have them
         if self.predictions is None:
-            self.classify_images()
+            self.get_images_batch()
 
-        self.transitions = list()
+        transitions = list()
 
-        # go thorough all the predictions
-        for i in range(len(self.predictions) - 1):
+        for i in range(len(self.predictions)):
 
-            # if we hit a lobby and just hit gameplay or meeting,
+            # if we experianced a transition
             if self.predictions[i] != self.predictions[i - 1]:
-                self.transitions.append((constants.label_ids[self.predictions[i]], i))
+                transitions.append((constants.label_ids[self.predictions[i]], i))
 
-        return self.transitions
+        return transitions
 
 
 def main():
@@ -164,9 +148,7 @@ def main():
     """
 
     collector = DataCollector("825004778")
-    transitions = collector.get_game_transitions()
-
-    print("transition indices: ", transitions)
+    collector.get_images_batch()
 
 
 if __name__ == "__main__":
