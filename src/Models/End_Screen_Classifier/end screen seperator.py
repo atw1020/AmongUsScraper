f4c96962c6541d78dea76033b5945132f24b7e16
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-01-18 21:43:44 -0800

    implemented prediction sorter

--- src/Models/End_Screen_Classifier/end screen seperator.py
+++ src/Models/End_Screen_Classifier/end screen seperator.py
@@ -4,6 +4,8 @@ Author: Arthur Wesley
 
 """
 
+import os
+
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
@@ -18,17 +20,28 @@ def main():
     :return:
     """
 
+    path = "Data/Winner Identifier/Test Data"
+
     model = load_model(constants.end_screen_classifier)
 
     # load the data from the directory
 
-    dataset = image_dataset_from_directory("Data/Winner Identifier/Training Data",
+    dataset = image_dataset_from_directory(path,
                                            image_size=constants.dimensions,
                                            shuffle=False)
 
     predictions = model.predict(dataset)
+    files = sorted(os.listdir(os.path.join(path, "ext")))
 
-    print(predictions)
+    # go through the predictions
+
+    for i in range(len(files)):
+
+        # check the prediction
+
+        if predictions[i][0] > predictions[i][1]:
+            os.rename(os.path.join(path, "ext", files[i]),
+                      os.path.join(path, files[i]))
 
 
 if __name__ == "__main__":
