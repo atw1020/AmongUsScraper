41d442135d35f3e410313cef44dc900dcdde1198
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-01-25 20:47:58 -0800

    created cropper for cropping meetings

--- src/Preprocessing/cropper.py
+++ src/Preprocessing/cropper.py
@@ -51,10 +51,42 @@ def crop_crewmates(image):
         (460, 5, 515, 80)
     ]
 
-    crops = []
+    crops = [
+        (85, 65, 305, 110),
+        (305, 65, 525, 110),
+        (85, 155, 305, 200),
+        (305, 155, 525, 200),
+        (85, 245, 305, 290),
+        (305, 245, 525, 290),
+        (85, 335, 305, 380),
+        (305, 335, 525, 380),
+        (85, 425, 305, 470),
+        (305, 425, 525, 470)
+    ]
 
     for i in range(len(boxes)):
 
+        crops.append(image.crop(boxes[i]))
+
+    return crops
+
+
+def crop_meeting(image):
+    """
+
+    crop the members out of the given meeting image
+
+    :param image: image to crop crewmates from
+    :return: cropped images
+    """
+
+    boxes = [
+        (),
+    ]
+
+    crops = []
+
+    for i in range(len(boxes)):
         crops.append(image.crop(boxes[i]))
 
     return crops
@@ -124,6 +156,7 @@ def main():
     :return:
     """
 
+    """
     crop_all_crewmates(os.path.join("Data",
                                     "Winner Identifier",
                                     "Test Data",
@@ -131,6 +164,18 @@ def main():
                        os.path.join("Data",
                                     "Crewmate Identifier",
                                     "Crude Data"))
+                                    """
+
+    image = Image.open(os.path.join("Data",
+                                    "Game Classifier",
+                                    "Training Data",
+                                    "Meeting",
+                                    "Meeting-841174127-979.jpg"))
+
+    crops = crop_meeting(image)
+
+    for i in range(len(crops)):
+        crops[i].save("image " + str(i) + ".jpg")
 
 
 if __name__ == "__main__":
