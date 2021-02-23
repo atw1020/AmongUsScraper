898465c1efed81d379de182729cac200e4fd6f36
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-02-22 16:01:10 -0800

    unable to train basic NN so got rid of batch size reduction

--- src/Models/Game_Classifier/trainer.py
+++ src/Models/Game_Classifier/trainer.py
@@ -44,7 +44,6 @@ def main():
     # print(os.path.exists("Data/Game Classifier/Training Data"))
 
     training_data = image_dataset_from_directory("Data/Game Classifier/Training Data",
-                                                 batch_size=16,
                                                  image_size=constants.dimensions)
     model = train_model(training_data)
 
