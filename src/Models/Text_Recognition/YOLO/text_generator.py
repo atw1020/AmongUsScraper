b6190049050f6707568fa6ee9e0a7225d0f6e0dd
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-03-04 11:18:53 -0800

    got model to predict

--- src/Models/Text_Recognition/YOLO/text_generator.py
+++ src/Models/Text_Recognition/YOLO/text_generator.py
@@ -7,25 +7,40 @@ Author: Arthur Wesley
 import os
 
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img
 
 from src import constants
+from src.Models.Text_Recognition import text_utils
+from src.Models.Text_Recognition.YOLO import data_generator
+from src.Models.Text_Recognition.YOLO.loss import YoloLoss
 from src.Models.Text_Recognition.YOLO.output_activation import YoloOutput
 
 
-def get_letters(img_path, model):
+def load():
+    """
+
+    load a model
+
+    :return:
+    """
+
+    return load_model(constants.letter_detection,
+                      custom_objects={"YoloOutput": YoloOutput,
+                                      "YoloLoss": YoloLoss})
+
+
+def get_letters(dataset,
+                model):
     """
 
     generate the letters that appear in on the img_path
 
-    :param img_path: the path to the image
+    dataset to get the letters from
+    :param dataset: dataset to get the letters for
     :param model: model to use
     :return: a string from the image
     """
 
-    image = load_img(img_path)
-
-    predictions = model.predict(image)
+    predictions = model.predict(dataset)
 
     # slice the predictions and save
     print(predictions.shape)
@@ -39,12 +54,13 @@ def main():
     :return:
     """
 
-    model = load_model(constants.letter_detection,
-                       custom_objects={"YoloOutput": YoloOutput})
+    vocab = text_utils.get_model_vocab()
 
-    files = os.listdir("Data/YOLO/Training Data")
+    dataset = data_generator.gen_dataset("Data/YOLO/Training Data",
+                                         vocab,
+                                         batch_size=1)
 
-    get_letters(files[0], model)
+    get_letters(dataset.take(1), load())
 
 
 if __name__ == "__main__":
