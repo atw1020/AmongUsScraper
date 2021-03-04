39be37d0141249eb79a06dd83cbb9598581e54bf
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-03-04 09:51:13 -0800

    update requirements.txt

--- src/Models/Text_Recognition/YOLO/text_generator.py
+++ src/Models/Text_Recognition/YOLO/text_generator.py
@@ -3,3 +3,19 @@
 Author: Arthur Wesley
 
 """
+
+from tensorflow.keras import Model
+from tensorflow.keras.preprocessing.image import load_img
+
+
+def get_letters(img_path):
+    """
+
+    generate the letters that appear in on the img_path
+
+    :param img_path: the path to the image
+    :return: a string from the image
+    """
+
+    image = load_img(img_path)
+
