11e7f3ec98eaa84b09dcdc6241d9a7c7f3138f8d
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-03-08 11:00:45 -0800

    updated tesseract.py

--- src/Models/Text_Recognition/tesseract.py
+++ src/Models/Text_Recognition/tesseract.py
@@ -4,7 +4,41 @@ Author: Arthur Wesley
 
 """
 
+import os
+
 from PIL import Image
 import pytesseract
 
 
+def read_text(image_path):
+    """
+
+    read the text from an image
+
+    :param image_path: path to the image
+    :return:
+    """
+
+    image = Image.open(image_path)
+
+    return pytesseract.image_to_string(image)
+
+
+def main():
+    """
+
+
+
+    :return:
+    """
+
+    path = "Data/YOLO/Training Data"
+    files = os.listdir(path)
+
+    for file in files:
+        print(read_text(os.path.join(path,
+                                     file)))
+
+
+if __name__ == "__main__":
+    main()
