23bff0c16278dff72728ee9b2491a35ab0fddeb8
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-04-01 13:19:09 -0700

    implemented loss 2

--- src/Models/Text_Recognition/YOLO/lossv2.py
+++ src/Models/Text_Recognition/YOLO/lossv2.py
@@ -4,6 +4,8 @@ Author: Arthur Wesley
 
 """
 
+import tensorflow as tf
+from tensorflow import math
 from tensorflow.keras.losses import Loss
 
 from src import constants
@@ -37,8 +39,11 @@ class YoloLoss(Loss):
         self.anchor_boxes = anchor_boxes
 
         # some local variables used to prevent repeat calculations
+
         self.l_obj = None
         self.l_no_obj = None
+
+        self.n_obj = None
 
     def call(self, y_true, y_pred):
         """
@@ -53,35 +58,68 @@ class YoloLoss(Loss):
         self.l_obj = None
         self.l_no_obj = None
 
+        self.n_obj = None
+
         return self.loss_1(y_true, y_pred) + self.loss_3(y_true, y_pred) + self.loss_3(y_true, y_pred)
 
     def loss_1(self, y_true, y_pred):
         """
 
-        calculates the first loss term
+        calculates the co ordinate loss
 
-        :param y_true:
-        :param y_pred:
+        :param y_true: true y
+        :param y_pred: predicted y
         :return:
         """
 
-        return 0
+        term_1 = math.square(y_true[:, :, :, :, 1:3] - y_pred[:, :, :, :, 1:3])
+        term_2 = math.square(math.sqrt(y_true[:, :, :, :, 3:5]) - math.sqrt(y_pred[:, :, :, :, 3:5]))
+
+        return self.lambda_co_ord / self.n_objects(y_true) * \
+               math.reduce_sum(term_1 + term_2 + self.l_object(y_true), -1)
 
     def loss_2(self, y_true, y_pred):
+        """
 
-        return 0
+        computes the class loss
+
+        :param y_true: true y
+        :param y_pred: predicted y
+        :return:
+        """
+
+        p     = y_true[:, :, :, :, 5:]
+        p_hat = y_pred[:, :, :, :, 5:]
+
+        log_error = p * math.log(p_hat)
+
+        return self.lambda_class / self.n_obj * \
+               math.reduce_sum(self.l_object(y_true) * log_error)
 
     def loss_3(self, y_true, y_pred):
 
         return 0
 
-    def l_object(self, y_true, y_pred):
+    def n_objects(self, y_true):
+        """
+
+
+
+        :param y_true:
+        :return:
+        """
+
+        if self.n_obj is None:
+            return
+
+        return self.n_obj
+
+    def l_object(self, y_true):
         """
 
         compute the variable "l_obj"
 
-        :param y_true:
-        :param y_pred:
+        :param y_true: the true values of y
         :return:
         """
 
@@ -102,7 +140,12 @@ class YoloLoss(Loss):
         :return:
         """
 
+        # todo: implement
+
         if self.l_no_obj is None:
+
+            # compute the highest probability
+
             self.l_no_obj = 0
 
         return self.l_noobj
